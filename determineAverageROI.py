38a07ffd85a25ff0b7fa64cb39e8e012bee888f8
Parker Burchett <parkerburchett@gmail.com>
2021-03-03 11:23:42 -0800

    Finished method to parse 3 User groups.

--- determineAverageROI.py
+++ determineAverageROI.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from collections import Counter
 # I think it makes the most sense to cast this in to a relational database for some basic quries.
 # Cast as .csv file
 #user[0] is rank
@@ -10,12 +11,13 @@ import pandas as pd
 
 
 class User:
-    # I don't think I will use this mode.
-    def __init__(self,rank, name, mode, corr, mmc, fnc, stake, day_1=None, month_3=None, year_1 =None):
+    # I don't think I will use this mehtod.
+    def __init__(self,rank, name, mode, corr, mmc, fnc, compute,stake, day_1=None, month_3=None, year_1 =None):
         self.rank = rank
         self.name = name
         self.mode = mode
         self.corr = corr
+        self.compute = compute
         self.mmc = mmc
         self.fnc =fnc
         self.stake = stake
@@ -35,10 +37,10 @@ def remove_new_lines(file):
         for l in lines_cleaned:
             out.write(l)
 
+
 def parse_user_from_raw_lines(record):
     """
-    Pass this 4 lines from the scores.txt file, it returns a tuple of that user's data. 
-    not rigerously tested
+    Old Verison You are not using this
     """
     rank = record[0]
     name = record[1]
@@ -71,8 +73,6 @@ def parse_user_from_raw_lines(record):
     return a_user
 
 
-# you need to parse by a line being single int. You can use try catch blocks for this. 
-
 def group_users(lines):
     """
     note this excludes the last user in the list. here it excludes 
@@ -93,8 +93,8 @@ def group_users(lines):
     start_index =0
     for line in lines[1:]:
         try:
-            rank = int(line)
-            if rank ==10001110101: # you are just excluding teh only user with a weird name
+            rank = int(line) # you are gettign groups by the rank as a single line
+            if rank == 10001110101: # you are just excluding the only user with a weird name. make a joke about this in teh tableau of this
                 raise Exception
             counter+=1
             lines_with_rank.append(counter)
@@ -118,30 +118,98 @@ def group_users(lines):
     len_5_users = [l for l in user_tupules if len(l) ==5]
     len_6_users = [l for l in user_tupules if len(l) ==6]
 
-    user_groups = [len_2_users, len_3_users ,len_4_users ,len_5_users ,len_6_users ]
-    for group in user_groups:
-        print(len(group))
-    # You should short this by lengths
-
-    print(user_tupules[-1])
-    print(len_2_users)
+    user_groups = [len_2_users, len_3_users ,len_4_users ,len_5_users ,len_6_users]
     return user_groups
 
+
+def parse_stats(stats):
+    """
+        Parse the stats for corr, mmc, fnc, stake, roi_1_day, roi,_3_month, roi_1_year.
+
+        since all the stats look the same you can store all the methods in a singel space.
+
+        Returns an array of floats
+
+    """
+    corr = stats[0]
+    mmc = stats[1]
+    fnc = stats[2]
+    if len(stats) == 3:
+        stake = 0
+        roi_1_day = 0
+        roi_3_months = 0
+        roi_1_year = 0
+    elif len(stats) == 4:
+        stake = stats[3].split(' ')[0] # untested this line removes the NMR
+        roi_1_day = 0
+        roi_3_months = 0
+        roi_1_year = 0
+    elif len(stats)==5:
+        stake = stats[3].split(' ')[0]
+        roi_1_day = stats[4]
+        roi_3_months = 0
+        roi_1_year = 0
+    elif len(stats)==6:
+        stake = stats[3].split(' ')[0]
+        roi_1_day = stats[4]
+        roi_3_months = stats[5]
+        roi_1_year = 0
+    elif len(stats)==7:
+        stake = stats[3].split(' ')[0]
+        roi_1_day = stats[4]
+        roi_3_months = stats[5]
+        roi_1_year = stats[6]
+
+    part_done_stats = [corr, mmc, fnc, stake, roi_1_day, roi_3_months, roi_1_year]
+    res =[]
+    for r in part_done_stats:
+        try:
+            r = round(float(r),6)
+            res.append(r)
+        except:
+            try:
+                r = r[:-1] # remove the % character
+                r = round(float(r)*.01,6)
+                res.append(r)
+            except:
+                res.append(0.0) # when they have a roi but no sake eg user35 UUAZED4
+    return res
+
+
 def parse_3_user_groups(users):
-    lengths =[len(a) for a in users]
+    """
+    This method takes in the user group with No modes and No compute and returns
+    a list of tupele for each person representign:
+    rank, usersname, Mode, if compute, corr, mmc, fnc, stake, roi_1_day, roi_3_months, roi_1_year
 
+    If they dont' have on of these value a 0 in inserted instead. 
 
-        
+    example:
+    (3673, 'OTOMARUKANTA', 'No Mode', 'No Compute', -0.096, -0.0958, -0.0967, 0.0, 0.0, 0.0, 0.0)
+    """
 
-
-
-
+    #counts = Counter([len(a[2]) for a in users])
+    uniform_user_list =[]
+    for a in users:
+        mode = 'No Mode' # defaults
+        compute ='No Compute' # defaults
+        rank =a[0]
+        name =a[1]
+        stats = a[2]
+        stat_tup = parse_stats(stats)
+        uniform_user = [int(rank[0]), name[0], mode, compute]
+        for s in stat_tup:
+            uniform_user.append(s)
+        uniform_user = tuple(uniform_user)
+        print(uniform_user)
+        uniform_user_list.append(uniform_user)
+    return uniform_user_list
 
 def main():
-    # scores was gathered on 3/2/2021
+    # scores.txt was gathered on 3/2/2021
 
     with open('cleaned_scores.txt','r') as fin:
         users = group_users(fin.readlines())
-        cleaned_3_group = parse_3_user_groups
+        cleaned_3_group = parse_3_user_groups(users[1])
        
 main()
