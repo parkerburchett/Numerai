751048e02b5ac44fee896d1802d78bcbdde3f77b
Parker Burchett <parkerburchett@gmail.com>
2021-03-05 13:23:48 -0800

    Added Graph methods to Notebook

--- First_Analysis.py
+++ First_Analysis.py
@@ -5,63 +5,93 @@ import intro_numerai_api as local_api
 import pingGraphQL
 import statsmodels.api as sm
 
-
-# I look at the csv file I created eariler to look at some regression and basic exploritaiy stats on the profitablity and other stats for the leader board. 
-
+# I look at the csv file I created eariler to look at some regression and basic exploritaiy stats on the profitablity and other stats for the leader board.
 
 # in theroy you can do this periodily to see the piciture over time
 
-# I dont' have a clean way of getting the data right now I just copy and past it to notepad. 
+# I dont' have a clean way of getting the data right now I just copy and past it to notepad.
 
 
-def create_scatter_plot(df, x_name='corrRep', y_name ='nmrStaked'):
-    """
-    Scatter Plot of Corrilation vs Stake
-
-    """
-    x = df[x_name]
-    y = df[y_name]
-    plt.scatter(x,y, s=.5)
-    plt.xlabel(x_name)
-    plt.ylabel(y_name)
-    plt.show()
-
-
-def create_histogram(df, col='nmrStaked', bins =50, log=True):
-    x=df[col]
-    plt.hist(x,bins, log=log)
+# you might want to show sevearl min stakes, as a range. eg create sevearl box plots all on one show
+def create_box_plot(df, col='corrRep', min_stake=0):
+    x = df[(df[col].notnull()) & (df['nmrStaked'] > min_stake)][col]
+    plt.boxplot(x, vert=False)
+    num_elements = x.count()
+    plt.title(
+        f'Distribution of {col} nmrStaked>{min_stake}\n Number of Elements:{num_elements}'
+    )
     plt.xlabel(col)
     plt.show()
 
 
-def custom_describe(df, col):
-    percents = [.1,.2,.3,.4,.5,.6,.7,.8,.9]
-    print(df[col].describe(percentiles=percents))
+# good
+def create_scatter_plot(df, x_name='corrRep', y_name='nmrStaked', min_stake=0):
+    x = df[(df[x_name].notnull()) & (df['nmrStaked'] > min_stake)][x_name]
+    y = df[(df[y_name].notnull()) & (df['nmrStaked'] > min_stake)][y_name]
+    plt.scatter(x, y, s=.5)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    num_elements = x.count()
+    plt.title(
+        f'{y_name} v {x_name} when nmrStaked>{min_stake}\n Number of Elements:{num_elements}'
+    )
+    plt.show()
 
 
-def compute_single_regression(df, independent_variable='corrRep', dependent_variable ='return_13Weeks'):
-    local_df =df[df[dependent_variable].notnull()]
-    print(local_df.columns)
-    x = local_df[independent_variable]
-    y = local_df[dependent_variable]
-    model =sm.OLS(y,x)
+# good
+def create_histogram(df, col='nmrStaked', bins=50, min_stake=0):
+    x = df[(df[x_name].notnull()) & (df['nmrStaked'] > min_stake)][x_name]
+    plt.hist(x, bins)
+    plt.xlabel(col)
+    num_elements = x.count()
+    plt.title(
+        f'Histogram of {col} when nmrStaked>{min_stake}\n Number of Elements:{num_elements}'
+    )
+    plt.show()
+
+
+# good
+def custom_describe(df, col='corrRep', min_stake=0):
+    percents = [.1, .2, .3, .4, .5, .6, .7, .8, .9]
+    x = df[(df[col].notnull()) & (df['nmrStaked'] > min_stake)][col]
+    print(f'{col} when nmrStaked>{min_stake}\n')
+    print(x.describe(percentiles=percents))
+
+
+# good
+def compute_single_regression(df,
+                              independent_variable='corrRep',
+                              dependent_variable='3M_returns',
+                              min_stake=0):
+
+
+    x = df[(df[dependent_variable].notnull())
+           & (df['nmrStaked'] > min_stake)][independent_variable]
+    y = df[(df[dependent_variable].notnull())
+           & (df['nmrStaked'] > min_stake)][dependent_variable]
+    model = sm.OLS(y, x)
     results = model.fit()
     print(results.summary())
-    
-def compute_multiple_regression(df, indepenent_variables= ['corrRep', 'fncRep', 'mmcRep', 'nmrStaked'], dependent_variable ='return_13Weeks'):
-    local_df =df[df[dependent_variable].notnull()]
-    x = local_df[indepenent_variables]
-    y = local_df[dependent_variable]
-    model =sm.OLS(y,x)
+
+
+# good
+def compute_multiple_regression(
+        df,
+        indepenent_variables=['corrRep', 'fncRep', 'mmcRep', 'nmrStaked'],
+        dependent_variable='3M_returns',
+        min_stake=0):
+    x = df[(df[dependent_variable].notnull())
+           & (df['nmrStaked'] > min_stake)][indepenent_variables]
+    y = df[(df[dependent_variable].notnull())
+           & (df['nmrStaked'] > min_stake)][dependent_variable]
+    model = sm.OLS(y, x)
     results = model.fit()
     print(results.summary())
 
 
 def main():
-    df = pingGraphQL.custom_ping_leaderboad()
-    print(df.columns)
-    compute_multiple_regression(df)
+    df = pingGraphQL.get_leaderboard()
+    custom_describe(df, min_stake=100)
 
 
 main()
-
--- pingGraphQL.py
+++ pingGraphQL.py
@@ -4,41 +4,33 @@ import requests
 import matplotlib.pyplot as plt
 
 
-def custom_ping_leaderboad():
+def get_leaderboard() -> pd.DataFrame:
     """
-    Use a custom graph QL query to get the load the leaderboard into a pandas dataframe.
-    Source: https://towardsdatascience.com/connecting-to-a-graphql-api-using-python-246dda927840
-
+        Ping the Numerai leaderboard and return a Pandas DataFrame of the contents.
+        https://numer.ai/tournament
     """
-
-    batch = 1000
-    endpoint = 'https://api-tournament.numer.ai/'
-
+    url = 'https://api-tournament.numer.ai/'
     query = """{ 
-	v2Leaderboard { # If you dont' limit it gets 4352. You might want to batch it 
+	v2Leaderboard {
 	    returns
         corrRep
         fncRep
         mmcRep
         username
         nmrStaked
-        returns # this is the 1d roi. 2.2 is 2.2 percent. It does not *100
+        returns 
         return_52Weeks
         return_13Weeks
         rank
 	    }  
     }"""
-    # this throws erros when you try and get the returns for weeks13 and weeks52.
-    # you should let them know.
-
-    r = requests.post(url=endpoint, json={'query': query})
+    r = requests.post(url=url, json={'query': query})
     json_data = json.loads(r.text)
-
     df_data = json_data['data']['v2Leaderboard']
     df = pd.DataFrame(df_data)
-    df.convert_dtypes()
+    df.convert_dtypes() # the auto casts the columns into what it thinks is the most appropriate 
     df['nmrStaked'] = pd.to_numeric(df['nmrStaked'])
+    df = df.rename(columns={'returns': '1D_returns', 'return_13Weeks': '3M_returns', 'return_52Weeks': '1Y_returns'})
     return df
 
 
-df = custom_ping_leaderboad()
